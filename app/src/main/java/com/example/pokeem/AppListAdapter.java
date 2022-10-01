2c7f3c9abd8cb15cd73c286cc6dce50df79919be
Ananay <ananaygupta23@gmail.com>
2022-10-01 23:32:56 +0530

    `home fragments added`

---
+++ app/src/main/java/com/example/pokeem/AppListAdapter.java
@@ -0,0 +1,4 @@
+package com.example.pokeem;
+
+public class AppListAdapter {
+}
---
+++ app/src/main/java/com/example/pokeem/FloatService.java
@@ -0,0 +1,44 @@
+package com.example.pokeem;
+
+
+import android.annotation.SuppressLint;
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.app.Service;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.graphics.PixelFormat;
+import android.os.Build;
+import android.os.IBinder;
+import android.os.PowerManager;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+
+
+import androidx.core.app.AppLaunchChecker;
+import androidx.core.app.NotificationCompat;
+
+import java.util.Calendar;
+import java.util.HashMap;
+import java.util.Locale;
+import java.util.concurrent.TimeUnit;
+
+public class FloatService{
+
+
+
+
+
+
+
+}
